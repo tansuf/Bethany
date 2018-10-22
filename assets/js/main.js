dfc185120a6c534ea4ba35ddd996123f8fe6ec84
Yusuf Lateef <yungprof.psvita1@gmail.com>
2018-10-22 12:23:52 +0100

    done with our team

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip()
+
+    $('a.dropdown-item.color1').mouseover(function() {
+        $(this).addClass('text-dark');
+    });
 });
+
+
